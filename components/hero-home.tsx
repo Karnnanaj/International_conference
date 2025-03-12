0dc9a872990a3e54bc4b0668bea0780f3b0ebb43
Karnnan AJ <karnnanaj2004@gmail.com>
2025-03-12 22:33:13 +0530

    cssc

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -10,14 +10,14 @@ export default function HeroHome() {
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-800),var(--color-indigo-600),var(--color-gray-800),var(--color-indigo-400),var(--color-gray-800))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
               AI-driven tools for product teams
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
-                className="mb-8 text-xl text-indigo-200/65"
+                className="mb-8 text-xl text-gray-800"
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
